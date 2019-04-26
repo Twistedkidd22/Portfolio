2cd5ce7ba1418f518eac54a2cb93f0677cf2cb63
Josh Tietz <joshtietz11@gmail.com>
2019-04-25 23:31:02 -0500

    added resume page skeleton

---
+++ josh-tietz/src/Components/resume.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resume = (props) => {
+    <div className="resume-container">
+        <div className="resume">
+        {/* pdf viewer for resume file */}
+        </div>
+    </div>
+} 
+
+export default Resume;
